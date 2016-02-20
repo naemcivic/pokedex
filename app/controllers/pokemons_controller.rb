e8d615c891f7c457bd4e8d391c70bad6c18b8960
naemcivic <naemcivic@gmail.com>
2016-02-20 10:17:55 -0500

    Modify action index and show in controller to respond to return JSON

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,14 +1,23 @@
 class PokemonsController < ApplicationController
 
   def index
-
     @pokemons = Pokemon.all
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @pokemons }
+    end
 
   end
 
   def show
 
     @pokemon = Pokemon.find(params[:id])
+
+    respond_to do |format|
+      format.html
+      format.json { render json: @pokemon }
+    end
 
   end
 
